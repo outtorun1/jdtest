fc9261b219c425c5ae2f1412c1789a4a4de71147
younggi <outtorun1@gmail.com>
2022-06-09 14:52:22 +0900

    test 0001

---
+++ jdtest/src/main/java/com/jdtest/JdtestApplication.java
@@ -0,0 +1,13 @@
+package com.jdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdtestApplication.class, args);
+	}
+
+}
---
+++ jdtest/src/test/java/com/jdtest/JdtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
